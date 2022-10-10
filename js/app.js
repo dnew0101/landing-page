c0b37eeeff073973ccd759fb8d1a606e2d585927
dnew0130 <DavidN988878@gmail.com>
2022-10-10 14:12:10 -0700

    feat - Dynamic navbar creator

--- js/app.js
+++ js/app.js
@@ -1,62 +1,7 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+//The following code is a dynamic way to recruit the number of navbar cards required to navigate the page
+const nodeList = document.querySelectorAll(".landing__container");
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+for(let i = 1; i <= nodeList.length; i++) {
+    document.getElementById("navbar__list").innerHTML += "<li>Section " + i + "</li>";
+    console.log("Section " + i);
+}
