6af380e0cf0ac0a017152f5b4d4729f6e531c4aa
dnew0130 <DavidN988878@gmail.com>
2022-10-10 23:09:35 -0700

    feat - Add Javascript to further manipulate DOM

--- js/app.js
+++ js/app.js
@@ -7,20 +7,21 @@ for (let i = 1; i <= nodeList.length; i++) {
 } //I could've also used .appendChild() in this loop, but felt that less code is better, simpler code
 
 
+let temp = 0; //This is a globally accessible variable that determines which section and button are active
+
 //The next lines of code use "Event Delegation" to sense a click event and then
 // auto scroll to the desired section of the DOM
 let navbarMenu = navbarList.children;
 function scrollOnClick(event) {
-    let temp;
     if (event.target.nodeName = 'li') {
         for (let i = 0; i < navbarMenu.length; i++) { //This for loop filters out which index of the navbar was clicked
             if (event.target.innerText == navbarMenu[i].innerText) {
                 temp = i;
-                console.log(temp);
                 break;
             }
         }
         //The temp variable is used to find the navbar's corresponding node
+        myActiveClass();
         nodeList[temp].scrollIntoView({
             behavior: "smooth",
             block: "start",
@@ -29,3 +30,30 @@ function scrollOnClick(event) {
     }
 }
 navbarList.addEventListener('click', scrollOnClick);
+
+
+
+//Active status for article sections
+function myActiveClass() {
+    let currentActiveClass = document.querySelector(".your-active-class");
+    let newActiveClass = nodeList[temp];
+    if (newActiveClass != currentActiveClass) {
+        currentActiveClass.classList.remove("your-active-class");
+        newActiveClass.parentNode.classList.add("your-active-class");
+        //after digging through the Web API database, I finally found that the .parentNode property
+        //was the most ideal way to append the class onto each section
+    }
+}
+
+
+//Active status for toggling button and section cards
+const button = document.getElementsByClassName('button')[0];
+
+        //any time the button or a list item is clicked, the list disappears/appears in mobile mode
+function toggleButton() {
+    navbarList.classList.toggle('active');
+}
+
+button.addEventListener('click', toggleButton);
+navbarList.addEventListener('click', toggleButton);
+console.log(nodeList[temp]);
